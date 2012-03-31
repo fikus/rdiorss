45896995ed5a0cd5f9d5baa59226abc8afa91379
Eric Fikus <eric.fikus@rd.io>
2012-03-31 08:39:34 -0700

    Turn off debug mode

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def search():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True
+#    app.debug = True
     app.run(host='0.0.0.0', port=port)
